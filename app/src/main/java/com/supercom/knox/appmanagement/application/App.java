63829f9642d1db279898b41cf4c8efa0f1d35d58
adi.matzliah <adim@supercom.com>
2022-08-08 18:40:48 +0300

    * handle an exception once the app get boot receiver but it admin  not active yet or any other possible reason *  Add Timber

--- app/src/main/java/com/supercom/knox/appmanagement/application/App.java
+++ app/src/main/java/com/supercom/knox/appmanagement/application/App.java
@@ -3,12 +3,22 @@ package com.supercom.knox.appmanagement.application;
 import android.app.Application;
 import android.content.IntentFilter;
 
+import com.supercom.knox.appmanagement.BuildConfig;
 import com.supercom.knox.appmanagement.reboot.RebootReceiver;
+
+import timber.log.Timber;
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        //initTimber();
         getApplicationContext().registerReceiver(new RebootReceiver(),new IntentFilter("com.supercom.reboot"));
     }
+
+/*    public void initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }*/
 }
--- app/src/main/java/com/supercom/knox/appmanagement/reboot/BootReceiver.java
+++ app/src/main/java/com/supercom/knox/appmanagement/reboot/BootReceiver.java
@@ -5,10 +5,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 
+import com.supercom.knox.appmanagement.KnoxDeviceManager;
+
+import timber.log.Timber;
+
 public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        try {
+            KnoxDeviceManager.setAllowPowerOffAndRestart(context, false); // prevent the user from rebooting the device once the boot is completed
+        } catch (Exception e) {
+            Timber.e("setAllowPowerOffAndRestart is failed: %s", e.getMessage());
+        }
         context.registerReceiver(new RebootReceiver(),new IntentFilter("com.supercom.reboot"));
     }
 }
