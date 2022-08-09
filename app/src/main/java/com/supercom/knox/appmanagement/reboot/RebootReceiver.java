ccad6a466786f89e72b96f99cd89c470b80f72a3
adi.matzliah <adim@supercom.com>
2022-08-09 13:36:37 +0300

    * change log

--- app/src/main/java/com/supercom/knox/appmanagement/reboot/RebootReceiver.java
+++ app/src/main/java/com/supercom/knox/appmanagement/reboot/RebootReceiver.java
@@ -3,15 +3,16 @@ package com.supercom.knox.appmanagement.reboot;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.supercom.knox.appmanagement.KnoxDeviceManager;
+
+import timber.log.Timber;
 
 public class RebootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i("RebootReceiver","onReceive");
+        Timber.i("RebootReceiver onReceive()");
         KnoxDeviceManager.reboot(context);
     }
 }
